860306705e447d741a86e00acf6e565f8cdb2937
wrk1231 <wrk1231@gmail.com>
2020-05-20 17:28:54 -0400

    Update SVMClassifier.py
    fix bugs

--- SVMClassifier.py
+++ SVMClassifier.py
@@ -4,29 +4,27 @@ from sklearn.svm import SVC
 
 class SVMClf(object):
 
-    def __init__(X, y, C = 1, kernal='linear'):
+    def __init__(self, X, y, C = 1, kernel='linear'):
         
         self.X = X ## X as data
         self.y = y ## y as target
         ## kernal and C are model specific in SVM
-        self.kernal = kernal
+        self.kernel = kernel
         self.C = C
-        self.model = SVC(kernel= self.kernal, C=self.C)
+        self.model = SVC(kernel= self.kernel, C=self.C)
 
         self.lowerBound = np.min(X) - np.std(X)
         self.upperBound = np.min(X) + np.std(X)
         self.new_X = np.linspace(self.lowerBound, self.upperBound, 500)
 
-        self.fig = plt.figure(figsize = (12,8))
-        self.ax = plt.axes()
 
         self.trained = False
 
-    def train_model():
+    def train_model(self):
         self.model.fit(X,y)
         self.trained = True
 
-    def get_parameters():
+    def get_parameters(self):
         if self.trained == True:
             self.support_vectors = self.model.support_vectors_
             self.w = self.model.coef_[0]
@@ -41,7 +39,10 @@ class SVMClf(object):
         else:
             pass;
 
-    def plot_model():
+    def plot_model(self):
+        self.fig = plt.figure(figsize = (12,8))
+        self.ax = plt.axes()
+
         if self.trained == True:
             self.ax.scatter(X[:, 0], X[:, 1], c=y, cmap='rainbow', alpha=0.5)
             self.ax.scatter(self.support_vectors[:,0], self.support_vectors[:,1], s = 100, facecolors='k')
